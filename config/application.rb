8cf3c8713973702359ff009671c8557a7d34ad3b
Kelvin Koh <there.are.no.mails@gmail.com>
2015-05-27 16:16:18 +0800

    Images now showing up dynamically

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Estuk
     config.active_record.raise_in_transactional_callbacks = true
     config.paperclip_defaults = {
         storage: :s3,
+        s3_host_name: "s3-ap-southeast-1.amazonaws.com",
         s3_credentials: {
             bucket: ENV['AWS_BUCKET'],
             access_key_id: ENV['AWS_ACCESS_KEY_ID'],
