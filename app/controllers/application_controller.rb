c1d0eab03db18f5a0d0d31842e6a5baab7a5c88b
Kelvin Koh <there.are.no.mails@gmail.com>
2015-05-27 15:05:09 +0800

    Added flash notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 	
 	rescue_from CanCan::AccessDenied do |exception|
-		redirect_to root_path
+		redirect_to root_path, notice: "You are not authorized to access this"
 	end
 end
